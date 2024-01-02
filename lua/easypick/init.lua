16bede556f227304bbfc8f08f6a9c35a014ea980
GitMurf <64155612+GitMurf@users.noreply.github.com>
2024-01-01 17:15:52 -0800

    chore: formatting updates

--- lua/easypick/init.lua
+++ lua/easypick/init.lua
@@ -1,31 +1,33 @@
-local has_telescope, _ = pcall(require, 'telescope')
-local previewers = require('easypick.previewers')
-local actions = require "easypick.actions"
-local pick = require "easypick.pick"
+local has_telescope, _ = pcall(require, "telescope")
+local previewers = require("easypick.previewers")
+local actions = require("easypick.actions")
+local pick = require("easypick.pick")
 
 if not has_telescope then
-	error('This plugin requires nvim-telescope/telescope.nvim')
+  error("This plugin requires nvim-telescope/telescope.nvim")
 end
 
 local setup = function(args)
-	local pickers = args.pickers
-	local picker_names = {}
-	for _, value in pairs(pickers) do
-		table.insert(picker_names, value.name)
-	end
-	local command_opts = {
-		nargs = "*",
-		complete = function()
-			-- return completion candidates as a list-like table
-			return picker_names
-		end
-	}
-	vim.api.nvim_create_user_command('Easypick', function(opts) pick.one(opts.args, pickers) end, command_opts)
+  local pickers = args.pickers
+  local picker_names = {}
+  for _, value in pairs(pickers) do
+    table.insert(picker_names, value.name)
+  end
+  local command_opts = {
+    nargs = "*",
+    complete = function()
+      -- return completion candidates as a list-like table
+      return picker_names
+    end,
+  }
+  vim.api.nvim_create_user_command("Easypick", function(opts)
+    pick.one(opts.args, pickers)
+  end, command_opts)
 end
 
 return {
-	setup = setup,
-	previewers = previewers,
-	actions = actions,
-	one_off = pick.one_off,
+  setup = setup,
+  previewers = previewers,
+  actions = actions,
+  one_off = pick.one_off,
 }
--- lua/easypick/pick.lua
+++ lua/easypick/pick.lua
@@ -1,121 +1,127 @@
-local telescope_pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
+local telescope_pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
 local conf = require("telescope.config").values
-local actions = require "easypick.actions"
-local themes = require "telescope.themes"
+local actions = require("easypick.actions")
+local themes = require("telescope.themes")
 
 local function all(pickers)
-	local opts = themes.get_dropdown({})
-	local picker_names = {}
-	for _, value in pairs(pickers) do
-		table.insert(picker_names, value.name)
-	end
-	telescope_pickers.new(opts, {
-		prompt_title = "Easypick",
-		finder = finders.new_table {
-			results = picker_names,
-		},
-		sorter = conf.generic_sorter(opts),
-		attach_mappings = actions.nvim_command("Easypick")
-	}):find()
+  local opts = themes.get_dropdown({})
+  local picker_names = {}
+  for _, value in pairs(pickers) do
+    table.insert(picker_names, value.name)
+  end
+  telescope_pickers
+    .new(opts, {
+      prompt_title = "Easypick",
+      finder = finders.new_table({
+        results = picker_names,
+      }),
+      sorter = conf.generic_sorter(opts),
+      attach_mappings = actions.nvim_command("Easypick"),
+    })
+    :find()
 end
 
 local function run_command(command)
-	local handle = io.popen(command)
+  local handle = io.popen(command)
 
-	if (handle == nil) then
-		print('could not run specified command:' .. command)
-		return
-	end
+  if handle == nil then
+    print("could not run specified command:" .. command)
+    return
+  end
 
-	local output = handle:read("*a")
+  local output = handle:read("*a")
 
-	handle:close()
+  handle:close()
 
-	return output
+  return output
 end
 
 local function parse_command_output(output)
-	local list = {}
+  local list = {}
 
-	-- check if output is a string
-	if type(output) ~= 'string' then
-		print('command did not return a string')
-		return
-	end
+  -- check if output is a string
+  if type(output) ~= "string" then
+    print("command did not return a string")
+    return
+  end
 
-	for token in string.gmatch(output, "[^%c]+") do
-		table.insert(list, token)
-	end
+  for token in string.gmatch(output, "[^%c]+") do
+    table.insert(list, token)
+  end
 
-	return list
+  return list
 end
 
 local function one(picker_name, pickers)
-	if picker_name == '' then
-		return all(pickers)
-	end
-	local command = ''
-	local previewer = {}
-	local action = function()
-		-- dont do anything
-	end
-	local entry_maker = function(entry)
-		return {
-			value = entry,
-			display = entry,
-			ordinal = entry,
-		}
-	end
-	local opts = {}
+  if picker_name == "" then
+    return all(pickers)
+  end
+  local command = ""
+  local previewer = {}
+  local action = function()
+    -- dont do anything
+  end
+  local entry_maker = function(entry)
+    return {
+      value = entry,
+      display = entry,
+      ordinal = entry,
+    }
+  end
+  local opts = {}
 
-	for _, value in pairs(pickers) do
-		if value.name == picker_name then
-			command = value.command
-			previewer = value.previewer
-			action = value.action
-			entry_maker = value.entry_maker
-			opts = value.opts
-		end
-	end
+  for _, value in pairs(pickers) do
+    if value.name == picker_name then
+      command = value.command
+      previewer = value.previewer
+      action = value.action
+      entry_maker = value.entry_maker
+      opts = value.opts
+    end
+  end
 
-	if command == '' then
-		print('picker with name ' .. picker_name .. ' does not exist')
-		return
-	end
+  if command == "" then
+    print("picker with name " .. picker_name .. " does not exist")
+    return
+  end
 
-	local result = run_command(command)
+  local result = run_command(command)
 
-	local list = parse_command_output(result)
+  local list = parse_command_output(result)
 
-	telescope_pickers.new(opts, {
-		prompt_title = picker_name,
-		finder = finders.new_table {
-			results = list,
-			entry_maker = entry_maker,
-		},
-		sorter = conf.generic_sorter(opts),
-		previewer = previewer,
-		attach_mappings = action
-	}):find()
+  telescope_pickers
+    .new(opts, {
+      prompt_title = picker_name,
+      finder = finders.new_table({
+        results = list,
+        entry_maker = entry_maker,
+      }),
+      sorter = conf.generic_sorter(opts),
+      previewer = previewer,
+      attach_mappings = action,
+    })
+    :find()
 end
 
 local function one_off(command)
-	local result = run_command(command)
+  local result = run_command(command)
 
-	local list = parse_command_output(result)
+  local list = parse_command_output(result)
 
-	telescope_pickers.new({}, {
-		prompt_title = command,
-		finder = finders.new_table {
-			results = list,
-		},
-		sorter = conf.generic_sorter({}),
-	}):find()
+  telescope_pickers
+    .new({}, {
+      prompt_title = command,
+      finder = finders.new_table({
+        results = list,
+      }),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
 end
 
 return {
-	all = all,
-	one = one,
-	one_off = one_off,
+  all = all,
+  one = one,
+  one_off = one_off,
 }
